c6c1a431d755fcad381b3c912d56974231537b0e
Lornavav <>
2023-08-06 14:33:48 +0000

    Add category model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,13 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -12,7 +19,7 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts")
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
-    category = models.ForeignKey('')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     cast = models.CharField(max_length=300)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -25,6 +32,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def number_of_likes(self):
         return self.likes.count()
