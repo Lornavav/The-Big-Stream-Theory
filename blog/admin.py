9357c56858f18bae7369ef0588dff94db386a298
Lornavav <>
2023-08-07 15:43:10 +0000

    Add filters and configure display on admin panal

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on', 'category')
+    search_fields = ['title', 'content', 'category']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on', 'category')
     summernote_fields = ('content')
