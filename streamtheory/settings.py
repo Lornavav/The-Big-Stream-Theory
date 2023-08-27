d1539086ebae2e9f1900812d94b17bf26bc592cd
Lornavav <>
2023-08-27 14:17:32 +0000

    Add feature to leave a comment & install crispy bootstrap5

--- streamtheory/settings.py
+++ streamtheory/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'django_summernote',
     'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
 ]
 
@@ -60,11 +61,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
