89ee87dfa802619eac94a9f06a9970f7f91f7064
Lornavav <>
2023-08-27 13:10:11 +0000

    Add articles link to navbar

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from .models import Comment
 from django import forms
 
 
-class CommentForm(forms.ModelForm:)
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
