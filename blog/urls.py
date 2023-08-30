70b5c9f75db526d6cf2739d82bbf2ee6ab339cbe
Lornavav <>
2023-08-30 20:51:34 +0000

    Wire up profile url

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='article_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('category/<category>/', views.CatListView.as_view(), name='category'),
+    path('profile/', views.MyProfile.as_view(), name='profile'),
 ]
