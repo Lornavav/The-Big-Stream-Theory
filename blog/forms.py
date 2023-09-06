5a011c455da429b73211ad5a5158fb1988d43fc3
Lornavav <>
2023-09-06 21:09:23 +0000

    Add rich text editor to BlogForm

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,11 @@ from .models import Comment, Profile, Post
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django_summernote.widgets import SummernoteWidget
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+
+
 
 
 class CommentForm(forms.ModelForm):
@@ -44,9 +47,9 @@ class BlogForm(LoginRequiredMixin, forms.ModelForm):
         exclude = ['author', 'slug']
 
         widgets = {
-            'title': forms.TextInput(attrs={"class": "form-control"}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
-            'cast': forms.TextInput(attrs={"class": "form-control"}),
-            'content': forms.Textarea(attrs={"class": "form-control"}),
+            'cast': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': SummernoteWidget(attrs={'summernote': {'width': '100%', 'height': '600px'}}),
             'status': forms.Select(attrs={'class': 'form-control'}),
-        }
+            }
